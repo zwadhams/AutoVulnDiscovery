4b05e59dc7b88209fb5ff4cf7b2603fa8bd30005
Ernesto <ernesto.ortizrey@student.montana.edu>
2024-09-10 10:03:57 -0600

    Changed the path used in the set_background script to use the current working directory command so that it can work on any machine.

--- Assignment1/CommandInjection/set_background.sh
+++ Assignment1/CommandInjection/set_background.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 
 # Download the image
-curl -o /home/brittanyboles/AutoVulnDiscovery/Assignment1/CommandInjection/image.jpg "https://www.montana.edu/assets/images/1gq3l/image15.jpg"
+curl -o "$(pwd)/image.jpg" "https://www.montana.edu/assets/images/1gq3l/image15.jpg"
 
-sleep 3
-# Set the background image
-
-gsettings set org.gnome.desktop.background picture-uri 'file:////home/brittanyboles/AutoVulnDiscovery/Assignment1/CommandInjection/image.jpg'
+# Set the downloaded image as the desktop background
+gsettings set org.gnome.desktop.background picture-uri "file://$(pwd)/image.jpg"
