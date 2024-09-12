b4dfd5ded2839b7aaf7c5333591038fab5cf7e03
Garrett Perkins <60898909+gperkz12@users.noreply.github.com>
2024-09-12 11:23:29 -0600

    Create safe.c

---
+++ Assignment1/CommandInjection/src/safe.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char **argv) {
+    if (argc != 2) {
+        fprintf(stderr, "Usage: %s <filename>\n", argv[0]);
+        return 1;
+    }
+
+    FILE *file = fopen(argv[1], "r");
+    if (file == NULL) {
+        perror("Error opening file");
+        return 1;
+    }
+
+    char ch;
+    while ((ch = fgetc(file)) != EOF) {
+        putchar(ch);
+    }
+
+    fclose(file);
+    return 0;
+}
