2658fd049656f4e5d70aefec49f1467ca411725e
Zach <33103466+zwadhams@users.noreply.github.com>
2024-10-17 13:25:15 -0600

    edits comments

--- Assignment3/symEx.py
+++ Assignment3/symEx.py
@@ -8,7 +8,7 @@ parser = Parser(C_LANG)
 
 # Example path: Assignment3\test_program.c
 def read_c_code_from_file(file_path):
-    with open(file_path, 'rb') as file:  # Read as bytes
+    with open(file_path, 'rb') as file: 
         return file.read()
     
 def print_tree(node, indent=""):
@@ -17,7 +17,6 @@ def print_tree(node, indent=""):
         print_tree(child, indent + "  ")
 
 def main():
-    # Parse command line arguments
     parser_args = argparse.ArgumentParser(description="Parse a C file using Tree-sitter.")
     parser_args.add_argument("c_file", help="Path to the C file to parse.")
     args = parser_args.parse_args()
@@ -25,7 +24,6 @@ def main():
     # Read C code from the file
     c_code = read_c_code_from_file(args.c_file)
 
-    # Parse the C code
     tree = parser.parse(c_code)
 
     # Get the root node of the syntax tree
