12b413b6e1f0733657a74664dde77687ed52d799
gperkz12 <gperkz12@gmail.com>
2024-11-20 12:32:36 -0700

    working

--- SemesterProject/uprobe.py
+++ SemesterProject/uprobe.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 from bcc import BPF
+import sys
 
 program = r"""
 int trace_main(struct pt_regs *ctx) {
@@ -10,8 +11,12 @@ int trace_main(struct pt_regs *ctx) {
 # Initialize BPF
 b = BPF(text=program)
 # Attach to the symbol `my_function` in the `test` binary
-binary_path = "./hello"
-symbol = "main"
+if len(sys.argv) != 3:
+    print("Usage: {} <binary_path> <symbol>".format(sys.argv[0]))
+    sys.exit(1)
+
+binary_path = sys.argv[1]
+symbol = sys.argv[2]
 syscall = b.get_syscall_fnname("execve")
 b.attach_uprobe(name=binary_path, sym=symbol, fn_name="trace_main")
 
